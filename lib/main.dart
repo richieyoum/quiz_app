2d641351668da00a54acff1e7d92b98b87ca1a7c
richie youmls <seongmin.youm@mail.mcgill.ca>
2021-06-17 02:19:31 -0400

    added main app page UI

---
+++ lib/main.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/questionList.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+void main() => runApp(QuizApp());
+
+class QuizApp extends StatelessWidget {
+  const QuizApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({Key? key}) : super(key: key);
+
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+Icon getScoreIcon({bool state: true}) {
+  if (state) {
+    return Icon(
+      Icons.check,
+      color: Colors.green,
+    );
+  }
+  return Icon(
+    Icons.close,
+    color: Colors.red,
+  );
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scores = [];
+  QuestionList questionList = new QuestionList();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Center(
+            child: Text(
+              questionList.getQuestionText(),
+              style: TextStyle(color: Colors.white, fontSize: 25.0),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.green),
+                onPressed: () {
+                  setState(() {
+                    scores.add(
+                        getScoreIcon(state: questionList.getAnswer() == true));
+                    questionList.getNext();
+                    if (questionList.isFinished()) {
+                      Alert(
+                          context: context,
+                          title: "Quiz is over!",
+                          desc: "Thanks for playing :)",
+                          buttons: [
+                            DialogButton(
+                                child: Text(
+                                  "Play again!",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                color: Colors.green,
+                                onPressed: () {
+                                  setState(() {
+                                    Navigator.pop(context);
+                                    questionList.resetIdx();
+                                    scores = [];
+                                  });
+                                })
+                          ]).show();
+                    }
+                  });
+                },
+                child: Text(
+                  'True',
+                  style: TextStyle(color: Colors.white),
+                )),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.red),
+                onPressed: () {
+                  setState(() {
+                    scores.add(
+                        getScoreIcon(state: questionList.getAnswer() == false));
+                    questionList.getNext();
+                    if (questionList.isFinished()) {
+                      Alert(
+                          context: context,
+                          title: "Quiz is over!",
+                          desc: "Thanks for playing :)",
+                          buttons: [
+                            DialogButton(
+                                child: Text(
+                                  "Play again!",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                color: Colors.green,
+                                onPressed: () {
+                                  setState(() {
+                                    Navigator.pop(context);
+                                    questionList.resetIdx();
+                                    scores = [];
+                                  });
+                                })
+                          ]).show();
+                    }
+                  });
+                },
+                child: Text(
+                  'False',
+                  style: TextStyle(color: Colors.white),
+                )),
+          ),
+        ),
+        Row(
+          children: scores,
+        )
+      ],
+    );
+  }
+}
